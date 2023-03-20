223565bd5765a95cb911f84c91b91e445e01205c
joaquin <joaquinahuel93@gmail.com>
2023-03-20 15:49:56 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const { inquirerMenu,
+    pausa,
+    leerInput,
+    } = require("./helpers/inquirer");
+const Busquedas = require("./models/busquedas");
+
+
+const main = async() => {
+
+    const busquedas = new Busquedas();
+    let opt = "";
+
+    do {
+        opt = await inquirerMenu();
+        
+        switch( opt ) {
+
+            case 1:
+                // Mostrar mensaje
+                const  lugar = await leerInput('Ciudad: ');
+                console.log(lugar);
+
+                // Buscar los lugares
+
+                // Seleccionar el lugar 
+
+                // Clima
+
+                // Mostrar resultados
+                console.log('\nInformación de la ciudad\n'.green);
+                console.log('Ciudad:', );
+                console.log('Lat:', );
+                console.log('Lng:', );
+                console.log('Temperatura:', );
+                console.log('Mínima:', );
+                console.log('Máxima:', );
+
+            break;
+
+        }
+        
+        if( opt !== 0 ) await pausa();
+       
+        
+      } while (opt !== 0);
+
+}
+
+main();
---
+++ models/busquedas.js
@@ -0,0 +1,23 @@
+
+
+class Busquedas {
+
+    historial = ['Tegucigalpa', 'Madrid', 'San José'];
+
+    constructor() {
+        
+    }
+
+    async ciudad( lugar = '') {
+
+        // peticion http
+        console.log(lugar);
+
+        return []; // retornar los lugares
+    }
+}
+
+
+
+
+module.exports = Busquedas;
